71bf9e54c31f9bdff8a1cc1ec2dc8185204d608d
eeee386 <attilasedon1@gmail.com>
2020-02-13 21:06:11 +0100

    Doubly Linked list deleted used Gfg soultion

--- CraftingInterpretersC/DoublyLinkedList.c
+++ CraftingInterpretersC/DoublyLinkedList.c
@@ -1,81 +1,144 @@
-#include <string.h>
-#include <stdio.h>
+// A complete working C program to demonstrate all insertion methods 
+#include <stdio.h> 
+#include <stdlib.h> 
 
-typedef struct Node {
-    struct Node* parent_node;
-    char value[100];
-    struct Node* child_node;
-} Node;
+// A linked list node 
+struct Node { 
+	int data; 
+	struct Node* next; 
+	struct Node* prev; 
+}; 
 
-typedef struct DoublyLinkedList {
-    struct Node* start_node;
-    struct Node* end_node;
-    int size;
-} DoublyLinkedList;
+/* Given a reference (pointer to pointer) to the head of a list 
+and an int, inserts a new node on the front of the list. */
+void push(struct Node** head_ref, int new_data) 
+{ 
+	/* 1. allocate node */
+	struct Node* new_node = (struct Node*)malloc(sizeof(struct Node)); 
 
-int delete(DoublyLinkedList list, char* value) {
-    struct Node* node = list.start_node;
-    while(node) {
-        if(strcmp(node->value, value)) {
-            struct Node* child_node = node->child_node;
-            struct Node* parent_node = node->parent_node;
-            parent_node->child_node = child_node;
-            child_node->parent_node = parent_node;
-            return 1;
-        }
-        node = node->child_node;
-    }
-    return 0;
-}
+	/* 2. put in the data */
+	new_node->data = new_data; 
 
-int insert(DoublyLinkedList list, char* value) {
-    Node newNode = {list.end_node, "", NULL};
-    strcpy(newNode.value, value);
-    if(!list.start_node){
-        list.start_node = &newNode;
-    }
-    list.size++;
-    return 1;
-}
+	/* 3. Make next of new node as head and previous as NULL */
+	new_node->next = (*head_ref); 
+	new_node->prev = NULL; 
 
-Node* find(DoublyLinkedList list, char* value){
-    struct Node* node = list.start_node;
-    printf("%p", node);
-    while(node) {
-        //printf("while runs");
-        if(strcmp(node->value, value)){
-            return node;
-        }
-        node = node->child_node;
-    }
-    return NULL;
-}
+	/* 4. change prev of head node to new node */
+	if ((*head_ref) != NULL) 
+		(*head_ref)->prev = new_node; 
 
+	/* 5. move the head to point to the new node */
+	(*head_ref) = new_node; 
+} 
 
-int main() {
-    DoublyLinkedList list = {NULL, NULL, 0};
-    insert(list, "1");
-    insert(list, "2");
-    insert(list, "3");
-    insert(list, "4");
-    insert(list, "5");
+/* Given a node as prev_node, insert a new node after the given node */
+void insertAfter(struct Node* prev_node, int new_data) 
+{ 
+	/*1. check if the given prev_node is NULL */
+	if (prev_node == NULL) { 
+		printf("the given previous node cannot be NULL"); 
+		return; 
+	} 
 
-    Node* node1 = find(list, "1");
-    printf("%s", &node1->value);
+	/* 2. allocate new node */
+	struct Node* new_node = (struct Node*)malloc(sizeof(struct Node)); 
 
-    /*Node* node2 = find(list, "2");
-    printf("%s", &node2->value);
+	/* 3. put in the data */
+	new_node->data = new_data; 
 
-    Node* node3 = find(list, "3");
-    printf("%s", &node3->value);
+	/* 4. Make next of new node as next of prev_node */
+	new_node->next = prev_node->next; 
 
-    Node* node4 = find(list, "4");
-    printf("%s", &node4->value);
+	/* 5. Make the next of prev_node as new_node */
+	prev_node->next = new_node; 
 
-    Node* node5 = find(list, "5");
-    printf("%s", &node5->value);
+	/* 6. Make prev_node as previous of new_node */
+	new_node->prev = prev_node; 
 
-    delete(list, "1");
-    find(list, "1");*/
-    return 0;
-}
+	/* 7. Change previous of new_node's next node */
+	if (new_node->next != NULL) 
+		new_node->next->prev = new_node; 
+} 
+
+/* Given a reference (pointer to pointer) to the head 
+of a DLL and an int, appends a new node at the end */
+void append(struct Node** head_ref, int new_data) 
+{ 
+	/* 1. allocate node */
+	struct Node* new_node = (struct Node*)malloc(sizeof(struct Node)); 
+
+	struct Node* last = *head_ref; /* used in step 5*/
+
+	/* 2. put in the data */
+	new_node->data = new_data; 
+
+	/* 3. This new node is going to be the last node, so 
+		make next of it as NULL*/
+	new_node->next = NULL; 
+
+	/* 4. If the Linked List is empty, then make the new 
+		node as head */
+	if (*head_ref == NULL) { 
+		new_node->prev = NULL; 
+		*head_ref = new_node; 
+		return; 
+	} 
+
+	/* 5. Else traverse till the last node */
+	while (last->next != NULL) 
+		last = last->next; 
+
+	/* 6. Change the next of last node */
+	last->next = new_node; 
+
+	/* 7. Make last node as previous of new node */
+	new_node->prev = last; 
+
+	return; 
+} 
+
+// This function prints contents of linked list starting from the given node 
+void printList(struct Node* node) 
+{ 
+	struct Node* last; 
+	printf("\nTraversal in forward direction \n"); 
+	while (node != NULL) { 
+		printf(" %d ", node->data); 
+		last = node; 
+		node = node->next; 
+	} 
+
+	printf("\nTraversal in reverse direction \n"); 
+	while (last != NULL) { 
+		printf(" %d ", last->data); 
+		last = last->prev; 
+	} 
+} 
+
+/* Drier program to test above functions*/
+int main() 
+{ 
+	/* Start with the empty list */
+	struct Node* head = NULL; 
+
+	// Insert 6. So linked list becomes 6->NULL 
+	append(&head, 6); 
+
+	// Insert 7 at the beginning. So linked list becomes 7->6->NULL 
+	push(&head, 7); 
+
+	// Insert 1 at the beginning. So linked list becomes 1->7->6->NULL 
+	push(&head, 1); 
+
+	// Insert 4 at the end. So linked list becomes 1->7->6->4->NULL 
+	append(&head, 4); 
+
+	// Insert 8, after 7. So linked list becomes 1->7->8->6->4->NULL 
+	insertAfter(head->next, 8); 
+
+	printf("Created DLL is: "); 
+	printList(head); 
+
+	getchar(); 
+	return 0; 
+} 
